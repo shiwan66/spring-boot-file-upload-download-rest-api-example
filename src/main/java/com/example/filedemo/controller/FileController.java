e64e0afc8a853b6685cf69467b059ad292f45124
shiwan66 <shiwan66@gmail.com>
2021-02-20 09:52:04 +0800

    添加跨域

--- src/main/java/com/example/filedemo/controller/FileController.java
+++ src/main/java/com/example/filedemo/controller/FileController.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin("*")
 public class FileController {
 
     private static final Logger logger = LoggerFactory.getLogger(FileController.class);
